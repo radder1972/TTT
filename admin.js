90903b79d7a12c684658356e016d3c3e4f0e2ba3
radder1972 <matthias.radder@gmail.com>
2026-06-03 07:45:37 +0200

    Automated True Time Thai update: admin.js

--- admin.js
+++ admin.js
@@ -424,7 +424,11 @@ function renderBookingsTable() {
         }
 
         // Add Print Invoice button to actions
-        const printBtnHtml = `<button type="button" class="btn btn-xs btn-outline btn-print-admin" onclick="printBookingInvoice('${b.id}')">🖨️ Printen</button>`;
+        const printBtnHtml = `<button type="button" class="btn btn-xs btn-outline btn-print-admin-icon" onclick="printBookingInvoice('${b.id}')" title="Print Factuur">
+            <svg class="icon-brand" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="width: 14px; height: 14px;">
+                <path d="M6 9V2h12v7M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2M6 14h12v8H6z"/>
+            </svg>
+        </button>`;
         if (actionsHtml === `<span class="text-muted">-</span>`) {
             actionsHtml = printBtnHtml;
         } else {
