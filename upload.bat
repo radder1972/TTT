6d0b7d195012013a81c65cad2d397ed6994a2e63
radder1972 <matthias.radder@gmail.com>
2026-06-03 17:00:37 +0200

    Delete upload.bat

--- upload.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0upload.ps1"
-if %errorlevel% neq 0 (
-    echo.
-    echo [X] Er is een fout opgetreden tijdens de uitvoering!
-    echo Druk op een toets om dit venster te sluiten...
-    pause > nul
-)
